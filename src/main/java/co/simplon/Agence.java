1e3593fbff6e5dfa12673cb0ceade02e929793c1
GinoChasles <gino.chasles@gmail.com>
2020-11-27 13:42:48 +0100

    ok

--- src/main/java/co/simplon/Agence.java
+++ src/main/java/co/simplon/Agence.java
@@ -3,60 +3,65 @@ package co.simplon;
 import java.util.ArrayList;
 
 public class Agence {
+  static Agence agenceNantes;
 
-    static Agence agenceNantes;
+  String nom;
+  String nomCommercial;
+  Adresse adresse;
+  ArrayList<Vehicule> stockDeVehicules;
 
-    String nom;
-    String nomCommercial;
-    Adresse adresse;
-    ArrayList<Vehicule> stockDeVehicules;
+  Agence(String nom, String nomCommercial) {
+    this.nom = nom;
+    this.nomCommercial = nomCommercial;
+    this.stockDeVehicules = new ArrayList<Vehicule>();
+  }
 
-    Agence(String nom, String nomCommercial) {
-        this.nom = nom;
-        this.nomCommercial = nomCommercial;
-        this.stockDeVehicules = new ArrayList<Vehicule>();
+  void ajouterAdresse(Adresse adresse) {
+    this.adresse = adresse;
+  }
+
+  void ajouterVehicule(Vehicule vehicule) {
+    this.stockDeVehicules.add(vehicule);
+  }
+
+  void afficherStock() {
+    System.out.println("Stock");
+    for (Vehicule vehicule : stockDeVehicules) {
+      System.out.println(vehicule);
+      vehicule.disponible = true;
     }
+  }
 
-    void ajouterAdresse(Adresse adresse) {
-        this.adresse =adresse;
-    }
+  public String toString() {
+    return (
+      "agence: " +
+      this.nom +
+      " - " +
+      this.nomCommercial +
+      " - " +
+      this.adresse.ville
+    );
+  }
 
-    void ajouterVehicule(Vehicule vehicule){
-        this.stockDeVehicules.add(vehicule);
-    }
+  static void creerAgenceNantes() {
+    agenceNantes = new Agence("Agence de Nantes", "Gino");
+    Adresse adresse = new Adresse("rue des moulins", "44200", "Nantes");
+    agenceNantes.ajouterAdresse(adresse);
+    System.out.println(agenceNantes);
 
-    void afficherStock(){
-        System.out.println("Stock");
-        for (Vehicule vehicule : stockDeVehicules) {
-            System.out.println(vehicule);
-            vehicule.disponible = true;
-        }
-    }
+    Voiture voitPetitPrix = new Voiture("Dacia", "blanc", 5, "essence", 30);
+    Voiture voitureChere = new Voiture("Audi", "rouge", 5, "essence", 75);
+    agenceNantes.ajouterVehicule(voitPetitPrix);
+    agenceNantes.ajouterVehicule(voitureChere);
+    // System.out.println(voitPetitPrix);
 
-    public String toString() {
-        return "agence: " + this.nom + " - " + this.nomCommercial
-                + " - " + this.adresse.ville;
-    }
+    agenceNantes.afficherStock();
+  }
 
-    static void creerAgenceNantes(){
-        agenceNantes = new Agence("Agence de Nantes", "Gino");
-        Adresse adresse = new Adresse("rue des moulins", "44200", "Nantes");
-        agenceNantes.ajouterAdresse(adresse);
-        System.out.println(agenceNantes);
-
-        Voiture voitPetitPrix = new Voiture("Dacia","blanc",5,"essence",30);
-        Voiture voitureChere = new Voiture("Audi","rouge",5,"essence",75);
-        agenceNantes.ajouterVehicule(voitPetitPrix);
-        agenceNantes.ajouterVehicule(voitureChere);
-        // System.out.println(voitPetitPrix);
-
-        agenceNantes.afficherStock();
-    }
-
-    static void reserverPremierVehicule(Client client) {
-        Vehicule voiture = agenceNantes.stockDeVehicules.get(0);
-        client.reservation = voiture;
-        voiture.disponible = false;
-        agenceNantes.afficherStock();
-    }
+  static void reserverPremierVehicule(Client client) {
+    Vehicule voiture = agenceNantes.stockDeVehicules.get(0);
+    client.reservation = voiture;
+    voiture.disponible = false;
+    agenceNantes.afficherStock();
+  }
 }
--- src/main/java/co/simplon/App.java
+++ src/main/java/co/simplon/App.java
@@ -4,22 +4,21 @@ package co.simplon;
  * Hello world!
  */
 public final class App {
-    private App() {
-    }
 
-   
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        Agence.creerAgenceNantes();
-        creerClientEtReserver();
-    }
+  private App() {}
 
-    static void creerClientEtReserver() {
-        Client clientRiche = new Client("Trump","0000");
-        Agence.reserverPremierVehicule(clientRiche);
-    }
+  /**
+   * Says hello to the world.
+   * @param args The arguments of the program.
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    Agence.creerAgenceNantes();
+    creerClientEtReserver();
+  }
+
+  static void creerClientEtReserver() {
+    Client clientRiche = new Client("Trump", "0000");
+    Agence.reserverPremierVehicule(clientRiche);
+  }
 }
